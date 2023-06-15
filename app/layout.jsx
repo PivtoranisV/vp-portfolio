b798abba4345639952a99f40f9ece3972424c602
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-06-15 22:49:59 +0400

    Project skeleton

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import Header from '@/components/Header';
+import './globals.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Volodymyr Pivtoranis',
+  description: 'Volodymyr Pivtoranis portfolio website',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="bg-[url('/starry-sky.jpg')]">
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
