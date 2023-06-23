8cd115bdbbccf1e5ed7187df87f21a0666730b98
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-06-23 21:54:02 +0400

    Added reusable component for animation

---
+++ components/Reveal.jsx
@@ -0,0 +1,46 @@
+'use client';
+import { useRef, useEffect } from 'react';
+import { motion, useInView, useAnimation } from 'framer-motion';
+
+const Reveal = ({ children }) => {
+  const ref = useRef();
+  const isInView = useInView(ref);
+  const textAnimation = useAnimation();
+  const backgroundAnimation = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      textAnimation.start('visible');
+      backgroundAnimation.start('visible');
+    }
+  }, [textAnimation, isInView, backgroundAnimation]);
+
+  return (
+    <div className="relative">
+      <motion.div
+        ref={ref}
+        variants={{
+          hidden: { opacity: 0, y: 100 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={textAnimation}
+        transition={{ duration: 0.7, delay: 0.25 }}
+      >
+        {children}
+      </motion.div>
+      <motion.div
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: '100%' },
+        }}
+        initial="hidden"
+        animate={backgroundAnimation}
+        transition={{ duration: 0.7, ease: 'easeIn' }}
+        className="absolute top-1 bottom-1 left-0 right-0 bg-secondary z-20"
+      />
+    </div>
+  );
+};
+
+export default Reveal;
--- components/Slogan.jsx
+++ components/Slogan.jsx
@@ -1,33 +1,12 @@
-'use client';
-import { useRef, useEffect } from 'react';
-import { motion, useInView, useAnimation } from 'framer-motion';
+import Reveal from './Reveal';
 
 const Slogan = () => {
-  const ref = useRef();
-  const isInView = useInView(ref);
-  const textAnimation = useAnimation();
-
-  useEffect(() => {
-    if (isInView) {
-      textAnimation.start('visible');
-    }
-  }, [textAnimation, isInView]);
-
   return (
-    <motion.section
-      ref={ref}
-      variants={{
-        hidden: { opacity: 0, y: 100 },
-        visible: { opacity: 1, y: 0 },
-      }}
-      initial="hidden"
-      animate={textAnimation}
-      transition={{ duration: 0.7, delay: 0.25 }}
-    >
+    <Reveal>
       <h1 className="mx-auto mt-[-80px] sm:mt-[0] bg-gradient-to-r from-blue-700 via-amber-200 to-yellow-500 bg-clip-text text-transparent font-black lg:text-[65px] sm:text-[50px] text-[40px] lg:leading-[98px] text-center w-[225px] sm:w-[280px] lg:w-[360px]">
         If you can DREAM IT, You can DO IT
       </h1>
-    </motion.section>
+    </Reveal>
   );
 };
 
