74217c5936c757e5588796df0c3c8f8479476d0f
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-06-28 21:43:19 +0400

    Added tilt to skills

--- components/Skills/SkillItem.jsx
+++ components/Skills/SkillItem.jsx
@@ -1,6 +1,8 @@
 'use client';
 import Image from 'next/image';
 import { useState } from 'react';
+import { Tilt } from 'react-tilt';
+import ZoomIn from '../Motions/ZoomIn';
 
 const SkillItem = ({ name, icon }) => {
   const [showName, setShowName] = useState(false);
@@ -14,20 +16,27 @@ const SkillItem = ({ name, icon }) => {
   };
 
   return (
-    <div
-      className="blue-yellow-gradient rounded-full p-1"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <div className="bg-tertiary rounded-full w-[100px] h-[100px] flex justify-center items-center">
-        <Image src={icon} alt={name} width={85} height={85} />
-        {showName && (
-          <div className="absolute bg-gray-400 text-black px-2 py-1 rounded mt-36">
-            {name}
+    <Tilt>
+      <ZoomIn>
+        <div
+          className="blue-yellow-gradient rounded-full p-1"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <div
+            className="bg-tertiary rounded-full w-[100px] h-[100px] flex justify-center items-center"
+            options={{ max: 45, scale: 1, speed: 450 }}
+          >
+            <Image src={icon} alt={name} width={85} height={85} />
+            {showName && (
+              <div className="absolute bg-gray-400 text-black px-2 py-1 rounded mt-36">
+                {name}
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </div>
+        </div>
+      </ZoomIn>
+    </Tilt>
   );
 };
 
