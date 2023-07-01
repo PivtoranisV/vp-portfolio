f4791922afd0498a7771bb928014617d6aa01f39
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-07-02 00:11:04 +0400

    Update project card style

--- components/Projects/ProjectItem.jsx
+++ components/Projects/ProjectItem.jsx
@@ -21,40 +21,42 @@ const ProjectItem = ({
           scale: 1,
           speed: 450,
         }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full h-full"
+        className="blue-yellow-gradient p-1 h-full rounded-2xl"
       >
-        <div className="flex justify-between text-[30px] sm:text-[40px] text-gray-400 mb-5">
-          <a href={github} target="_blank">
-            <AiFillGithub />
-          </a>
-          <a href={demo} target="_blank">
-            <AiFillEye />
-          </a>
-        </div>
-        <div>
-          <Image
-            src={picture}
-            width={250}
-            height={250}
-            alt={title}
-            className="w-full h-full object-cover rounded-2xl"
-          />
-        </div>
-        <div className="mt-5">
-          <h3 className="text-white font-bold text-[24px]">{title}</h3>
-          <p className="mt-2 text-gray-400 text-[14px] text-justify">
-            {description}
-          </p>
-        </div>
-        <div className="mt-4 flex flex-wrap gap-2">
-          {tags.map((tag) => (
-            <p
-              key={`${title}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
-            >
-              #{tag.name}
+        <div className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full h-full">
+          <div className="flex justify-between text-[30px] sm:text-[40px] text-gray-400 mb-5">
+            <a href={github} target="_blank">
+              <AiFillGithub />
+            </a>
+            <a href={demo} target="_blank">
+              <AiFillEye />
+            </a>
+          </div>
+          <div>
+            <Image
+              src={picture}
+              width={250}
+              height={250}
+              alt={title}
+              className="w-full h-full object-cover rounded-2xl"
+            />
+          </div>
+          <div className="mt-5">
+            <h3 className="text-white font-bold text-[24px]">{title}</h3>
+            <p className="mt-2 text-gray-400 text-[14px] text-justify">
+              {description}
             </p>
-          ))}
+          </div>
+          <div className="mt-4 flex flex-wrap gap-2">
+            {tags.map((tag) => (
+              <p
+                key={`${title}-${tag.name}`}
+                className={`text-[14px] ${tag.color}`}
+              >
+                #{tag.name}
+              </p>
+            ))}
+          </div>
         </div>
       </Tilt>
     </FadeIn>
