11f966293ca48fa5e9bc105c35591c788e8fca73
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-07-04 23:41:25 +0400

    Added form handling functionality

--- components/Contacts.jsx
+++ components/Contacts.jsx
@@ -1,9 +1,56 @@
+'use client';
+import { useState } from 'react';
 import { IoLocationSharp } from 'react-icons/io5';
 import { AiOutlineMail, AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
-import ZoomIn from './Motions/ZoomIn';
+import emailjs from '@emailjs/browser';
 import FadeIn from './Motions/FadeIn';
 
 const Contacts = () => {
+  const [form, setForm] = useState({ name: '', email: '', message: '' });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const inputHandler = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs
+      .send(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: 'Volodymyr Pivtoranis',
+          from_email: form.email,
+          to_email: 'pivtoranis87@gmail.com',
+          message: form.message,
+        },
+        process.env.NEXT_PUBLIC_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setIsLoading(false);
+          alert('Thank you. I will get back to you as soon as possible.');
+
+          setForm({
+            name: '',
+            email: '',
+            message: '',
+          });
+        },
+        (error) => {
+          setIsLoading(false);
+          console.error(error);
+
+          alert('Ahh, something went wrong. Please try again.');
+        }
+      );
+  };
+
   return (
     <section id="contacts">
       <h1 className="px-2 font-black text-gray-400 lg:text-[65px] sm:text-[50px] text-[40px] lg:leading-[98px] mt-20 uppercase tracking-wider">
@@ -56,33 +103,39 @@ const Contacts = () => {
             <h3 className="text-center font-black text-secondary text-[35px] uppercase tracking-wider">
               What Can I Do For You?
             </h3>
-            <form className="mt-12 flex flex-col">
+            <form className="mt-12 flex flex-col" onSubmit={submitHandler}>
               <label className="text-white font-medium"> Your Name</label>
               <input
                 type="text"
                 name="name"
+                value={form.name}
                 placeholder="What's your good name?"
                 className="py-4 px-6 placeholder:text-gray-400 text-secondary rounded-lg outline-none border-none font-medium mb-6"
+                onChange={inputHandler}
               />
               <label className="text-white font-medium">Your Email</label>
               <input
                 type="email"
                 name="email"
+                value={form.email}
                 placeholder="What's your web address?"
                 className="py-4 px-6 placeholder:text-gray-400 text-secondary rounded-lg outline-none border-none font-medium mb-6"
+                onChange={inputHandler}
               />
               <label className="text-white font-medium">Your Message</label>
               <textarea
                 rows={5}
                 name="message"
+                value={form.message}
                 placeholder="What you want to say?"
                 className="py-4 px-6 placeholder:text-gray-400 text-secondary rounded-lg outline-none border-none font-medium mb-6"
+                onChange={inputHandler}
               />
               <button
                 type="submit"
                 className="py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-inner shadow-secondary"
               >
-                Send
+                {isLoading ? 'Sending' : 'Send'}
               </button>
             </form>
           </div>
